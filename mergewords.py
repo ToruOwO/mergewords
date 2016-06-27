3f14d6e87291375f5a454f275bddc698af8d4fd0
ToruOwO <toorumon@gmail.com>
2016-06-27 19:42:19 +0900

    1
    T____T
    
    “405 Method Not Allowed
    
    The method POST is not allowed for this resource. “

--- mergewords.py
+++ mergewords.py
@@ -2,7 +2,6 @@
 
 import webapp2
 
-
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
@@ -12,7 +11,7 @@ class MainPage(webapp2.RequestHandler):
         	<head><title>Word Machine</title></head>
         	<body>
         	 <h1>Word Machine</h1>
-	          <form action="" method="post">
+	          <form action="" method="post" action="/magic">
 	        	<input type="text" name="first"><br>
 	        	<input type="text" name="second"><br>
 	        	<p><input type="submit" value="Magic!"></p>
@@ -22,13 +21,23 @@ class MainPage(webapp2.RequestHandler):
 
 class Merge(webapp2.RequestHandler):
 	def post(self):
-		a = self.request.get("first")
-		b = self.request.get("second")
+		a = self.request.get('first')
+		b = self.request.get('second')
+		c = ''
+		i = 0
+		while i < max(len(a),len(b)):
+			if not a[i]:
+				c = c + b[i]
+			elif not b[i]:
+				c = c + a[i]
+			else:
+				c = c + a[i] + b[i]
+			i+=1
 		res = """<html><body>
 				{}
-				</body></html>""".format(a+b)
+				</body></html>""".format(c)
 		self.response.headers["Content-Type"] = "text/html"
-		self.response.write(res)
+		self.response.out.write(res)
 
 routes = [('/', MainPage),('/magic', Merge)]
 
